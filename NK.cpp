f86b871514d1769e9fc05298a7058462be4fb7d2
yuvasmart <37366739+yuvasmart@users.noreply.github.com>
2018-07-24 11:11:07 +0530

    Create NK.cpp

---
+++ NK.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n;
+	int k,sum=0;
+	cin>>n;
+	cin>>k;
+	int *arr=new int[n];
+	for(int i=0;i<n;i++)
+	{
+     cin>>arr[i];
+	}
+	for(int j=0;j<k;j++)
+	{
+	   sum = sum+arr[j];
+	}
+	cout<<sum;
+	return 0;
+}

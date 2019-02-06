c871c63643130ac11cd7489c11c18427a630cbd3
yuvasmart <37366739+yuvasmart@users.noreply.github.com>
2019-02-06 10:48:41 +0530

    Update roman.py

--- roman.py
+++ roman.py
@@ -1,6 +1,6 @@
-def king(s):
+def king(s)
     val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    inc = 0
+   inc=0
     for i in range(len(s)):
         if i > 0 and val[s[i]] > val[s[i - 1]]:
                 inc += val[s[i]] - 2 * val[s[i - 1]]

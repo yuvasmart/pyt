0df0ce1feb6e92a382bb1bbbb9263e18e2c46d48
yuvasmart <37366739+yuvasmart@users.noreply.github.com>
2019-02-06 09:57:23 +0530

    Update roman.py

--- roman.py
+++ roman.py
@@ -5,7 +5,7 @@ def roman(s):
         if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
                 int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
         else:
-                int_val += rom_val[s[i]]
-    return int_val
-string = input("Enter the roman no")
+            int_val +=rom_val[s[i]]
+     return int_val
+string = input("enter the roman")
 print(roman(string))
